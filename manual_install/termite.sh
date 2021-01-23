ea01059420929bb5d3c1a06c5ff990414c421767
Joris <joris.perrenet@gmail.com>
2021-01-23 23:00:37 +0100

    added the font to work on startup

--- manual_install/termite.sh
+++ manual_install/termite.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-cd home/$USER/Documents
+sudo apt install -y g++ libgtk-3-dev gtk-doc-tools gnutls-bin \
+    valac intltool libpcre2-dev libglib3.0-cil-dev libgnutls28-dev \
+    libgirepository1.0-dev libxml2-utils gperf build-essential
 
 git clone https://github.com/ls4154/termite-ubuntu
 cd termite-ubuntu
